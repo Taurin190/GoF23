53c127cbdee3cd07538aa5022755b8fa591dd9df
Koichi Taura <taura.koichi@gmail.com>
2020-03-03 20:36:38 +0900

    enrich iterators bookshelfiterator test

--- 1.iterator/src/test/java/BookShelfIteratorTest.java
+++ 1.iterator/src/test/java/BookShelfIteratorTest.java
@@ -9,6 +9,8 @@ import org.mockito.MockitoAnnotations;
 public class BookShelfIteratorTest {
     @Mock
     private BookShelf bookShelfMock;
+    @Mock
+    private Book bookMock;
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
@@ -18,5 +20,24 @@ public class BookShelfIteratorTest {
         when(bookShelfMock.getLength()).thenReturn(1);
         BookShelfIterator iterator = new BookShelfIterator(bookShelfMock);
         assertTrue(iterator.hasNext());
+    }
+    @Test
+    public void testNext() throws Exception {
+        when(bookMock.getName()).thenReturn("test book mock");
+        when(bookShelfMock.getBookAt(0)).thenReturn(bookMock);
+        BookShelfIterator iterator = new BookShelfIterator(bookShelfMock);
+        Book actual = (Book) iterator.next();
+        assertEquals("test book mock", actual.getName());
+    }
+    @Test
+    public void testHasNextAfterNext() throws Exception {
+        when(bookShelfMock.getLength()).thenReturn(1);
+        when(bookMock.getName()).thenReturn("test book mock");
+        when(bookShelfMock.getBookAt(0)).thenReturn(bookMock);
+        BookShelfIterator iterator = new BookShelfIterator(bookShelfMock);
+        assertTrue(iterator.hasNext());
+        Book actual = (Book) iterator.next();
+        assertEquals("test book mock", actual.getName());
+        assertFalse(iterator.hasNext());
     }
 }
