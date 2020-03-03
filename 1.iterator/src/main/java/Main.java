94b62d04279bde81314070bfb323b317fbb32b65
Koichi Taura <taura.koichi@gmail.com>
2020-03-03 20:44:55 +0900

    add main class to use bookshelf and iterator

--- 1.iterator/src/main/java/Main.java
+++ 1.iterator/src/main/java/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        BookShelf bookShelf = new BookShelf();
+        bookShelf.appendBook(new Book("増補改訂版Java言語で学ぶデザインパターン入門"));
+        bookShelf.appendBook(new Book("数学ガールの秘密ノート/ビットとバイナリー (数学ガールの秘密ノートシリーズ)"));
+        bookShelf.appendBook(new Book("新版C言語プログラミングレッスン 入門編"));
+        bookShelf.appendBook(new Book("増補改訂版 Java言語で学ぶデザインパターン入門 マルチスレッド編"));
+
+        Iterator iterator = bookShelf.iterator();
+        while (iterator.hasNext()) {
+            Book book = (Book) iterator.next();
+            System.out.println(book.getName());
+        }
     }
 }
