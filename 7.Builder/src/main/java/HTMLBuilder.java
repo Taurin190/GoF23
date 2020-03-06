fceddd4f957556d89b2b73f27b55c8f859b6dc29
Koichi Taura <taura.koichi@gmail.com>
2020-03-07 07:52:56 +0900

    add HTMLBuilder implementation

--- 7.Builder/src/main/java/HTMLBuilder.java
+++ 7.Builder/src/main/java/HTMLBuilder.java
@@ -1,21 +1,60 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class HTMLBuilder extends Builder {
+    private String filename;
     @Override
     public void makeTitle(String title) {
-
+        filename = title + ".html";
+        File file = new File(filename);
+        try {
+            FileWriter filewriter = new FileWriter(file, true);
+            filewriter.append("<html><head><title>" + title + "</title></head><body>\n");
+            filewriter.append("<h1>" + title + "</h1>\n");
+            filewriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     @Override
     public void makeString(String str) {
-
+        File file = new File(filename);
+        try {
+            FileWriter filewriter = new FileWriter(file, true);
+            filewriter.append("<p>" + str + "</p>\n");
+            filewriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     @Override
     public void makeItems(String[] items) {
-
+        File file = new File(filename);
+        try {
+            FileWriter filewriter = new FileWriter(file, true);
+            filewriter.append("<ul>\n");
+            for (int i = 0; i < items.length; i++) {
+                filewriter.append("<li>" + items[i] + "</li>\n");
+            }
+            filewriter.append("</ul>\n");
+            filewriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     @Override
     public void close() {
-
+        File file = new File(filename);
+        try {
+            FileWriter filewriter = new FileWriter(file, true);
+            filewriter.append("</body></html>\n");
+            filewriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     public String getResult() {
-        return "";
+        return filename;
     }
 }
--- 7.Builder/src/test/java/HTMLBuilderTest.java
+++ 7.Builder/src/test/java/HTMLBuilderTest.java
@@ -1,6 +1,8 @@
+import org.junit.After;
 import org.junit.Test;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,78 +10,86 @@ import java.nio.file.Paths;
 import static org.junit.Assert.assertEquals;
 
 public class HTMLBuilderTest {
+    private String title = "Hello";
     @Test
     public void testGetResult() throws Exception {
         HTMLBuilder builder = new HTMLBuilder();
-        builder.makeTitle("Hello");
+        builder.makeTitle(title);
         String actual = builder.getResult();
         assertEquals("Hello.html", actual);
     }
     @Test
     public void testMakeTitle() throws Exception {
         HTMLBuilder builder = new HTMLBuilder();
-        builder.makeTitle("Hello");
+        builder.makeTitle(title);
         String filename = builder.getResult();
         Path file = Paths.get(filename);
         try (BufferedReader br = Files.newBufferedReader(file)) {
             String actual = br.readLine();
-            assertEquals("<html><head><title>Hello</title></head><body>\n", actual);
+            assertEquals("<html><head><title>Hello</title></head><body>", actual);
             actual = br.readLine();
-            assertEquals("<h1>Hello</h1>\n", actual);
+            assertEquals("<h1>Hello</h1>", actual);
         }
     }
     @Test
     public void testMakeString() throws Exception {
         HTMLBuilder builder = new HTMLBuilder();
-        builder.makeTitle("Hello");
+        builder.makeTitle(title);
         builder.makeString("World");
         String filename = builder.getResult();
         Path file = Paths.get(filename);
         try (BufferedReader br = Files.newBufferedReader(file)) {
             String actual = br.readLine();
-            assertEquals("<html><head><title>Hello</title></head><body>\n", actual);
+            assertEquals("<html><head><title>Hello</title></head><body>", actual);
             actual = br.readLine();
-            assertEquals("<h1>Hello</h1>\n", actual);
+            assertEquals("<h1>Hello</h1>", actual);
             actual = br.readLine();
-            assertEquals("<p>World</p>\n", actual);
+            assertEquals("<p>World</p>", actual);
         }
     }
     @Test
     public void testMakeItems() throws Exception {
         HTMLBuilder builder = new HTMLBuilder();
-        builder.makeTitle("Hello");
+        builder.makeTitle(title);
         builder.makeItems(new String[]{"HogeHoge", "FugaFuga"});
         String filename = builder.getResult();
         Path file = Paths.get(filename);
         try (BufferedReader br = Files.newBufferedReader(file)) {
             String actual = br.readLine();
-            assertEquals("<html><head><title>Hello</title></head><body>\n", actual);
+            assertEquals("<html><head><title>Hello</title></head><body>", actual);
             actual = br.readLine();
-            assertEquals("<h1>Hello</h1>\n", actual);
+            assertEquals("<h1>Hello</h1>", actual);
             actual = br.readLine();
-            assertEquals("<ul>\n", actual);
+            assertEquals("<ul>", actual);
             actual = br.readLine();
-            assertEquals("<li>HogeHoge</li>\n", actual);
+            assertEquals("<li>HogeHoge</li>", actual);
             actual = br.readLine();
-            assertEquals("<li>FugaFuga</li>\n", actual);
+            assertEquals("<li>FugaFuga</li>", actual);
             actual = br.readLine();
-            assertEquals("</ul>\n", actual);
+            assertEquals("</ul>", actual);
         }
     }
     @Test
     public void testMakeClose() throws Exception {
         HTMLBuilder builder = new HTMLBuilder();
-        builder.makeTitle("Hello");
+        builder.makeTitle(title);
         builder.close();
         String filename = builder.getResult();
         Path file = Paths.get(filename);
         try (BufferedReader br = Files.newBufferedReader(file)) {
             String actual = br.readLine();
-            assertEquals("<html><head><title>Hello</title></head><body>\n", actual);
+            assertEquals("<html><head><title>Hello</title></head><body>", actual);
             actual = br.readLine();
-            assertEquals("<h1>Hello</h1>\n", actual);
+            assertEquals("<h1>Hello</h1>", actual);
             actual = br.readLine();
-            assertEquals("</body></html>\n", actual);
+            assertEquals("</body></html>", actual);
+        }
+    }
+    @After
+    public void teardown() {
+        File file = new File(title + ".html");
+        if (file.exists()) {
+            file.delete();
         }
     }
 }
