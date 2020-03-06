ef1a2e336547eabb450ebafac7c4a496b6769c6b
Koichi Taura <taura.koichi@gmail.com>
2020-03-06 21:41:50 +0900

    add TextBuilder implementation

--- 7.Builder/src/main/java/TextBuilder.java
+++ 7.Builder/src/main/java/TextBuilder.java
@@ -1,22 +1,27 @@
 public class TextBuilder extends Builder {
+    private StringBuffer buffer = new StringBuffer();
     @Override
     public void makeTitle(String title) {
-
+        buffer.append("==============================\n");
+        buffer.append("「" + title + "」\n\n");
     }
     @Override
     public void makeString(String str) {
-
+        buffer.append("■ " + str + "\n\n");
     }
     @Override
     public void makeItems(String[] items) {
-
+        for (int i = 0; i < items.length; i++) {
+            buffer.append("・" + items[i] + "\n");
+        }
+        buffer.append("\n");
     }
 
     @Override
     public void close() {
-
+        buffer.append("==============================\n");
     }
     public String getResult() {
-        return "";
+        return buffer.toString();
     }
 }
