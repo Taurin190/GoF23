3579607f3ba934263a0d405781731614db587a11
Koichi Taura <taura.koichi@gmail.com>
2020-03-08 07:45:26 +0900

    add concrete display of test and implementation

--- 9.Bridge/src/main/java/StringDisplayImpl.java
+++ 9.Bridge/src/main/java/StringDisplayImpl.java
@@ -1,16 +1,31 @@
+import java.nio.charset.Charset;
+
 public class StringDisplayImpl extends DisplayImpl {
+    private String text;
+    private int length;
+    public StringDisplayImpl(String text) {
+        this.text = text;
+        length = text.getBytes(Charset.forName("Shift_JIS")).length;
+    }
     @Override
     public void rawOpen() {
-
+        printLine();
     }
 
     @Override
     public void rawPrint() {
-
+        System.out.println("|" + text + "|");
     }
 
     @Override
     public void rawClose() {
-
+        printLine();
+    }
+    private void printLine() {
+        System.out.print("+");
+        for (int i = 0; i < length; i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
     }
 }
--- 9.Bridge/src/test/java/StringDisplayImplTest.java
+++ 9.Bridge/src/test/java/StringDisplayImplTest.java
@@ -1,2 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
 public class StringDisplayImplTest {
+    private PrintStream defaultPrintStream;
+    private ByteArrayOutputStream byteArrayOutputStream;
+    @Before
+    public void setup() {
+        defaultPrintStream = System.out;
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(new BufferedOutputStream(byteArrayOutputStream)));
+    }
+    @Test
+    public void testRawOpen() throws Exception {
+        StringDisplayImpl stringDisplay = new StringDisplayImpl("Hello World.");
+        stringDisplay.rawOpen();
+        System.out.flush();
+        String actual = byteArrayOutputStream.toString();
+        assertEquals("+------------+\n", actual);
+    }
+    @Test
+    public void testRawPrint() throws Exception {
+        StringDisplayImpl stringDisplay = new StringDisplayImpl("Hello World.");
+        stringDisplay.rawPrint();
+        System.out.flush();
+        String actual = byteArrayOutputStream.toString();
+        assertEquals("|Hello World.|\n", actual);
+    }
+    @Test
+    public void testRawClose() throws Exception {
+        StringDisplayImpl stringDisplay = new StringDisplayImpl("Hello World.");
+        stringDisplay.rawClose();
+        System.out.flush();
+        String actual = byteArrayOutputStream.toString();
+        assertEquals("+------------+\n", actual);
+    }
 }
