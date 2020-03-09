83ba310ce099aa6d5b3c57b6b81c6013b956fad9
Koichi Taura <taura.koichi@gmail.com>
2020-03-09 21:26:27 +0900

    add player test and implementation of strategy pattern

--- 10.Strategy/src/main/java/Player.java
+++ 10.Strategy/src/main/java/Player.java
@@ -1,2 +1,31 @@
 public class Player {
+    private String name;
+    private Strategy strategy;
+    private int gameCount = 0;
+    private int winCount = 0;
+    private int loseCount = 0;
+    public Player(String name, Strategy strategy) {
+        this.name = name;
+        this.strategy = strategy;
+    }
+    public Hand nextHand() {
+        return strategy.nextHand();
+    }
+    public void win() {
+        strategy.study(true);
+        winCount++;
+        gameCount++;
+    }
+    public void lose() {
+        strategy.study(false);
+        loseCount++;
+        gameCount++;
+    }
+    public void even() {
+        strategy.study(false);
+        gameCount++;
+    }
+    public String toString() {
+        return "[" + name + ": " + gameCount + " games, " + winCount + " win, " + loseCount + " lose]";
+    }
 }
--- 10.Strategy/src/test/java/PlayerTest.java
+++ 10.Strategy/src/test/java/PlayerTest.java
@@ -1,20 +1,58 @@
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class PlayerTest {
+    @Mock
+    private Strategy strategy;
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
     @Test
     public void testNextHand() {
-
+        Player player = new Player("test", strategy);
+        when(strategy.nextHand()).thenReturn(Hand.getHand(Hand.HAND_VALUE_GUU));
+        Hand actual = player.nextHand();
+        assertEquals(Hand.getHand(Hand.HAND_VALUE_GUU), actual);
     }
     @Test
     public void testWin() {
-
+        Player player = new Player("test", strategy);
+        player.win();
+        verify(strategy, times(1)).study(true);
     }
     @Test
     public void testLose() {
-
+        Player player = new Player("test", strategy);
+        player.lose();
+        verify(strategy, times(1)).study(false);
     }
     @Test
     public void testEven() {
-
+        Player player = new Player("test", strategy);
+        player.even();
+        verify(strategy, times(1)).study(false);
+    }
+    @Test
+    public void testToString() {
+        Player player = new Player("test", strategy);
+        for (int i = 0; i < 3; i++) {
+            player.win();
+        }
+        for (int i = 0; i < 5; i++) {
+            player.lose();
+        }
+        for (int i = 0; i < 7; i++) {
+            player.even();
+        }
+        String actual = player.toString();
+        assertEquals("[test: 15 games, 3 win, 5 lose]", actual);
     }
 }
