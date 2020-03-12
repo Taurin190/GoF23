fc1208c7d2c45c7ff329fc1cc2ccd4c1585bc487
Koichi Taura <taura.koichi@gmail.com>
2020-03-12 18:32:20 +0900

    add test for database of facade pattern

--- 15.Facade/src/test/java/DatabaseTest.java
+++ 15.Facade/src/test/java/DatabaseTest.java
@@ -1,10 +1,34 @@
+import org.junit.Before;
 import org.junit.Test;
 
+import javax.xml.crypto.Data;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Properties;
 
+import static org.junit.Assert.assertEquals;
+
 public class DatabaseTest {
+    private PrintStream defaultPrintStream;
+    private ByteArrayOutputStream byteArrayOutputStream;
+    @Before
+    public void setup() {
+        defaultPrintStream = System.out;
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(new BufferedOutputStream(byteArrayOutputStream)));
+    }
     @Test
     public void testGetProperties() {
-
+        Properties properties = Database.getProperties("database");
+        String actual = properties.getProperty("test");
+        assertEquals("ok", actual);
+    }
+    @Test
+    public void testGetPropertiesWithInvalidFilename() {
+        Properties properties = Database.getProperties("invalid");
+        System.out.flush();
+        String actual = byteArrayOutputStream.toString();
+        assertEquals("Warning: invalid.txt is not found.\n", actual);
     }
 }
