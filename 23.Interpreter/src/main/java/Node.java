626282b4513018d48a7bfa9aa1b9fff88b8732b2
Koichi Taura <taura.koichi@gmail.com>
2020-03-18 21:27:03 +0900

    add implementation of Interpreter pattern

--- 23.Interpreter/src/main/java/Node.java
+++ 23.Interpreter/src/main/java/Node.java
@@ -1,5 +1,3 @@
-import java.text.ParseException;
-
 public abstract class Node {
     public abstract void parse(Context context) throws ParseException;
 }
--- 23.Interpreter/src/main/java/ProgramNode.java
+++ 23.Interpreter/src/main/java/ProgramNode.java
@@ -1,8 +1,12 @@
-import java.text.ParseException;
-
 public class ProgramNode extends Node {
+    private Node commandListNode;
     @Override
     public void parse(Context context) throws ParseException {
-
+        context.skipToken("program");
+        commandListNode = new CommandListNode();
+        commandListNode.parse(context);
+    }
+    public String toString() {
+        return "[program " + commandListNode + "]";
     }
 }
